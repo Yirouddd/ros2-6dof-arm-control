2970b1b0e917dd25b2b5a3475a6314670ccb459b
Yirou Dong <dongyirou.uk@gmail.com>
2026-05-22 20:09:27 +0100

    Add subscriber for commanded arm joint angles

--- src/arm_pkg/arm_pkg/arm_joint_node.py
+++ src/arm_pkg/arm_pkg/arm_joint_node.py
@@ -8,12 +8,25 @@ class ArmJointNode(Node):
     def __init__(self):
         super().__init__('arm_joint_node')
 
+        # Current arms state
+        self.current_joint_angles = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+        # Publisher
         self.publisher_ = self.create_publisher(
             ArmJointAngles,
             'arm_joint_angles',
             10
         )
 
+        # Subscriber
+        self.subscriber_ = self.create_subscription(
+            ArmJointAngles,
+            'cmd_arm_joint_angles',
+            self.subscriber_callback,
+            10
+        )
+
+        # Timer
         self.timer = self.create_timer(
             1.0,
             self.timer_callback
@@ -21,9 +34,18 @@ class ArmJointNode(Node):
 
         self.get_logger().info('Arm Joint Node Started')
 
+    def subscriber_callback(self, msg):
+
+        if len(msg.angles) != 6:
+            self.get_logger().error('Received invalid joint angles length')
+            return
+
+        self.current_joint_angles = list(msg.angles)
+        self.get_logger().info(f'Received: {self.current_joint_angles}')
+
     def timer_callback(self):
         msg = ArmJointAngles()
-        msg.angles = [0.0, 10.0, 20.0, 30.0, 40.0, 50.0]
+        msg.angles = self.current_joint_angles
 
         self.publisher_.publish(msg)
 
